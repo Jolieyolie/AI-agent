79a21215a63f358fa6b0310dacfbea69ab4ed84c
jolieyolie <congyi.h.f@gmail.com>
2025-06-11 11:55:06 +0200

    update call & message

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+if len(sys.argv) < 2:
+    print("Error:Please enter a prompt")
+    sys.exit(1)
 
+user_prompt = sys.argv[1]
 client = genai.Client(api_key=api_key)
 
-result = client.models.generate_content(model = "gemini-2.0-flash-001", contents ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(result.text)
-print(f"Prompt tokens: {result.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {result.usage_metadata.candidates_token_count}")
+#result = client.models.generate_content(model = "gemini-2.0-flash-001", contents ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+#client.models.generate_content(model = "gemini-2.0-flash-001", contents = prompt)
 
 
-if len(sys.argv) < 2:
-    print("Error:Please enter aprompt")
-    sys.exit(1)
-prompt = sys.argv[1]
-client.models.generate_content(model = "gemini-2.0-flash-001", contents = prompt)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+)
+
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
